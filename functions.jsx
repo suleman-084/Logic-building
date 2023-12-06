b93b3058158d0cf685a3fe379475fd6a2284cde3
suleman-084 <sallusk084@gmail.com>
2023-12-06 17:49:51 +0530

    solved the area of hexagon

--- functions.jsx
+++ functions.jsx
@@ -9,3 +9,15 @@ const power = (a,b) => {
 }
 const results = power(5,3)
 console.log("power is", results);
+
+
+// Given length of a regular hexagon, your function should return area of the hexagon.
+// Example:
+// Input: areaOfHexagon(10) ––> Output: 259.80
+
+const areaOfHexagon = (side) => {
+    const area = (3 * Math.sqrt(3)/2) * Math.pow(side,2)
+    return area.toFixed(2)
+}
+const output = areaOfHexagon(10)
+console.log("area is", output);
