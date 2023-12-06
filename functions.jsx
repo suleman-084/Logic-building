3682773cfa0db90bc4874c05b36a638b0cb6be1e
suleman-084 <sallusk084@gmail.com>
2023-12-06 17:38:50 +0530

    solved 1st problem of functions

---
+++ functions.jsx
@@ -0,0 +1,11 @@
+// Given a and b, your function should return the value of ab
+// Example:
+// Input: power(2,3) ––> Output: 8
+
+
+const power = (a,b) => {
+    return Math.pow(a,b)
+
+}
+const results = power(5,3)
+console.log("power is", results);
