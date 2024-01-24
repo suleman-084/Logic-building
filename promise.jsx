88d3167c2fc705cbbf540c072bc0aedc9ad6cf23
suleman-084 <sallusk084@gmail.com>
2024-01-24 12:25:47 +0530

    promise.any() problem solved

--- promise.jsx
+++ promise.jsx
@@ -6,9 +6,17 @@ const promise2 = Promise.reject(2);
 const promise3 = Promise.resolve(3);
 
 Promise.all([promise1, promise2, promise3])
-  .then((results) => {
-    console.log("All promises fulfilled:", results); // [1, 2, 3]
-  })
-  .catch((error) => {
-    console.error("One of the promises rejected:", error);
-  })
+    .then((results) => {
+        console.log("All promises fulfilled:", results); // [1, 2, 3]
+    })
+    .catch((error) => {
+        console.error("One of the promises rejected:", error);
+    })
+
+// promise.any
+
+promise1.finally([promise1, promise2, promise3]).then((res)=> {
+    console.log("resolved promise", res);
+}).catch((rej)=>{
+    console.error("all promises rejected",rej)
+})
