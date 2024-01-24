cc20d0965719403cc293890a7bfd02e85c562a3f
suleman-084 <sallusk084@gmail.com>
2024-01-24 12:37:02 +0530

    promise.race() problem solved

--- promise.jsx
+++ promise.jsx
@@ -20,3 +20,15 @@ promise1.finally([promise1, promise2, promise3]).then((res)=> {
 }).catch((rej)=>{
     console.error("all promises rejected",rej)
 })
+
+// promise.race
+const p1 = Promise.resolve("Error 1");
+const p2 = Promise.resolve("Error 2");
+
+Promise.race([promise1, promise2])
+  .then((winner) => {
+    console.log("First promise to settle", winner); // "Winner 2"
+  })
+  .catch((reason) => {
+    console.error("This should not happen:", reason);
+  });
