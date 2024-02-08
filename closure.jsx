1a46373fb32fe98ceefe00459499d33d567ed9f3
suleman-084 <sallusk084@gmail.com>
2024-02-08 23:12:42 +0530

    closure  definition added and solved a problem

--- closure.jsx
+++ closure.jsx
@@ -1,10 +1,27 @@
-// what will be logged to console
-let count = 0;
-(function printCount() {
-    if(count === 0) {
-        let count = 1 ;
-        console.log(count)
+const generateMultiplier = (factor) => {
+    return (number) => {
+        return number * factor
     }
-    console.log(count);
-})
-//1 first & 0
+}
+const multiplyByTwo = generateMultiplier(2)
+const multiplyByThree = generateMultiplier(3)
+
+console.log(multiplyByTwo(5))
+console.log(multiplyByThree(5))
+
+
+// definition :  when an inner func remembers the scope of its parent that forms a closure
+
+
+
+
+// what will be logged to console
+// let count = 0;
+// (function printCount() {
+//     if(count === 0) {
+//         let count = 1 ;
+//         console.log(count)
+//     }
+//     console.log(count);
+// })
+// //1 first & 0
