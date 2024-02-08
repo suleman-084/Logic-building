444a902ada44072fb2a1c0a2cc0321e7ee57ea2a
suleman-084 <sallusk084@gmail.com>
2024-02-09 01:11:19 +0530

    spread operator problem solved

--- rest&spread.jsx
+++ rest&spread.jsx
@@ -8,3 +8,9 @@ const numbers = (...numbers) => {
 }
 
 console.log(numbers(1, 2, 3, 4, 5))
+
+//spread: is used to spread an element of an array or object into another array or object
+
+const a = [1 , 2]
+const b =- [3 , 4 ]
+const c = [...a, ...b]
